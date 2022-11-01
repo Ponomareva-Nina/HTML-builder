6dd6546a58b86b5b57124c65f5fad876719be3a8
Nina Ponomareva <nina.9412@mail.ru>
2022-11-01 22:25:53 +0300

    refactor: minor refactoring, change goodbye message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,20 +3,19 @@ const path = require('path');
 const { stdin, stdout } = process;
 const input = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
-stdout.write('To end session write "exit"\n');
-stdout.write('To start input please write your text below:\n');
+stdout.write('Please write your text below \nor enter "exit" if you want to end session \n');
 stdin.on('data', data => {
   if(data.toString().trim() === 'exit') {
-    stdout.write('Ok, bye!');
+    stdout.write('Goodbye and have a nice day!');
     process.exit();
   } else {
     input.write(data);
     stdout.write('Success! Your text was added to file\n');
-    stdout.write('To continue input please write your text below:\n');
+    stdout.write('To continue input please write your text below \nor enter "exit" if you want to end session \n');
   }
 });
 
 process.on('SIGINT', function() {
-  console.log('Ok, bye!');
+  console.log('Goodbye and have a nice day!');
   process.exit();
 });
