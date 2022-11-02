285fa149239b8af0dc93abb4e31ab8ae0facab96
Nina Ponomareva <nina.9412@mail.ru>
2022-11-02 13:42:17 +0300

    feat: add solution for task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+const dir = path.join(__dirname, 'secret-folder');
+
+fs.readdir(dir, {withFileTypes: true}, (err, files) => {
+  if (err) throw err;
+  files.forEach(file => {
+    if (file.isFile()) {
+      fs.stat(path.join(dir, file.name), (err, stats) => {
+        if (err) throw err;
+        let sizeInKb = stats.size / 1024;
+        stdout.write(file.name.split('.')[0] + ' - ' + path.extname(file.name).slice(1) + ' - ' + sizeInKb + ' kb\n');
+      });
+    }
+  });
+});
